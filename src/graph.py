3e20b552786d6b39117b848c8699e4f6e137cd96
bernatsampera <b.sampera@gmail.com>
2025-09-28 12:27:28 +0200

    feat: mock research events found

--- src/graph.py
+++ src/graph.py
@@ -5,8 +5,8 @@ from langgraph.graph import END, START, StateGraph
 from langgraph.types import Command
 from src.llm_service import model_for_tools
 from src.prompts import lead_researcher_prompt
+from src.research_events.research_events_graph import research_events_app
 from src.state import (
-    CategoriesWithEvents,
     FinishResearchTool,
     ResearchEventsTool,
     SupervisorState,
@@ -36,8 +36,6 @@ async def supervisor_node(
     llm_with_tools = model_for_tools.bind_tools(tools)
 
     prompt = [("system", prompt)]
-
-    print("summary", prompt)
 
     response = await llm_with_tools.ainvoke(prompt)
 
@@ -90,22 +88,33 @@ async def supervisor_tools_node(
             )
 
         elif tool_name == "ResearchEventsTool":
-            prompt = tool_args["research_question"]
-            # result = await research_events_app.ainvoke(
-            #     prompt=prompt, existing_events=existing_events
-            # )
-            # combined_events = result["combined_events"]
-            result = ""
-
-            mockCombinedEvents = (
-                CategoriesWithEvents(
-                    early="- Henry Valentine Miller was born in New York City on December 26, 1891.\n- He lived at 450 East 85th Street in Manhattan during his early years.\n- His family moved to Williamsburg, Brooklyn when he was around nine years old, and later to Bushwick.\n- Miller attended Eastern District High School in Williamsburg.\n- He briefly studied at the City College of New York.\n- Miller became active with the Socialist Party of America.\n- He admired Hubert Harrison.",
-                    personal="- Miller married Beatrice Sylvas Wickens in 1917 and divorced her in 1923. They had a daughter, Barbara.\n- Miller met June Mansfield around 1924 and they married on June 1, 1924.\n- Miller lived with Kronski at some point between 1926-1927.\n- Miller moved to Paris in 1930. He spent several months there with June in 1938. During his ten-year stay in Paris, Miller became fluent in French.\n- Miller returned to New York in 1940 and moved to California in 1942, initially residing just outside Hollywood in Beverly Glen before settling in Big Sur in 1944.\n- Miller married Janina Martha Lepska in 1944 and had two children with her. They divorced in 1952.\n- Miller married Eve McClure in 1953 but they divorced in 1960.\n- Miller married Hiroko Tokuda in 1967 but they divorced in 1977.",
-                    career="- Miller quit Western Union to dedicate himself to writing in 1924.\n- He was supported financially by Roland Freedman who paid June Mansfield to write a novel, pretending it was her work and reviewing Miller's writing weekly.\n- Miller moved to Paris unaccompanied in 1930.\n- He was employed as a proofreader for the Chicago Tribune Paris edition in 1931 thanks to Alfred Perlès.\n- This period marked a creative time for Miller, and he began building a network of authors around Villa Seurat.\n- Lawrence Durrell became a lifelong friend.\n- Anaïs Nin and Hugh Guiler financially supported Miller between 1931-1934, covering his living expenses including rent at 18 Villa Seurat.\n- Nin became his lover and financed the first printing of Tropic of Cancer in 1934 with money from Otto Rank.",
-                    legacy="- Miller was nominated for the Nobel Prize in Literature by University of Copenhagen professor Allan Philip in 1973.\n- Miller participated in the filming of Reds in the late 1970s.\n- Miller held an ongoing correspondence of over 1,500 letters with Brenda Venus between 1978 and 1981.\n- Miller died on June 7, 1980 at his home in Pacific Palisades, Los Angeles, aged 88.\n- The Henry Miller Memorial Library was founded in Big Sur in 1981 by Emil White.",
-                ),
+            research_question = tool_args["research_question"]
+            result = await research_events_app.ainvoke(
+                research_question=research_question, existing_events=existing_events
             )
-            combined_events = mockCombinedEvents
+            combined_events = result["combined_events"]
+            # structured_llm = model_for_structured.with_structured_output(
+            #     CategoriesWithEvents
+            # )
+
+            # prompt = "Add some events to the existing events that response to the research question: {research_question}. Existing events: {existing_events}"
+            # prompt = prompt.format(
+            #     research_question=research_question, existing_events=existing_events
+            # )
+
+            # result = await structured_llm.ainvoke(prompt)
+
+            # print("result", result)
+
+            # mockCombinedEvents = (
+            #     CategoriesWithEvents(
+            #         early="- Henry Valentine Miller was born in New York City on December 26, 1891.\n- He lived at 450 East 85th Street in Manhattan during his early years.\n- His family moved to Williamsburg, Brooklyn when he was around nine years old, and later to Bushwick.\n- Miller attended Eastern District High School in Williamsburg.\n- He briefly studied at the City College of New York.\n- Miller became active with the Socialist Party of America.\n- He admired Hubert Harrison.",
+            #         personal="- Miller married Beatrice Sylvas Wickens in 1917 and divorced her in 1923. They had a daughter, Barbara.\n- Miller met June Mansfield around 1924 and they married on June 1, 1924.\n- Miller lived with Kronski at some point between 1926-1927.\n- Miller moved to Paris in 1930. He spent several months there with June in 1938. During his ten-year stay in Paris, Miller became fluent in French.\n- Miller returned to New York in 1940 and moved to California in 1942, initially residing just outside Hollywood in Beverly Glen before settling in Big Sur in 1944.\n- Miller married Janina Martha Lepska in 1944 and had two children with her. They divorced in 1952.\n- Miller married Eve McClure in 1953 but they divorced in 1960.\n- Miller married Hiroko Tokuda in 1967 but they divorced in 1977.",
+            #         career="- Miller quit Western Union to dedicate himself to writing in 1924.\n- He was supported financially by Roland Freedman who paid June Mansfield to write a novel, pretending it was her work and reviewing Miller's writing weekly.\n- Miller moved to Paris unaccompanied in 1930.\n- He was employed as a proofreader for the Chicago Tribune Paris edition in 1931 thanks to Alfred Perlès.\n- This period marked a creative time for Miller, and he began building a network of authors around Villa Seurat.\n- Lawrence Durrell became a lifelong friend.\n- Anaïs Nin and Hugh Guiler financially supported Miller between 1931-1934, covering his living expenses including rent at 18 Villa Seurat.\n- Nin became his lover and financed the first printing of Tropic of Cancer in 1934 with money from Otto Rank.",
+            #         legacy="- Miller was nominated for the Nobel Prize in Literature by University of Copenhagen professor Allan Philip in 1973.\n- Miller participated in the filming of Reds in the late 1970s.\n- Miller held an ongoing correspondence of over 1,500 letters with Brenda Venus between 1978 and 1981.\n- Miller died on June 7, 1980 at his home in Pacific Palisades, Los Angeles, aged 88.\n- The Henry Miller Memorial Library was founded in Big Sur in 1981 by Emil White.",
+            #     ),
+            # )
+            combined_events = result
             existing_events = combined_events
             all_tool_messages.append(
                 ToolMessage(
